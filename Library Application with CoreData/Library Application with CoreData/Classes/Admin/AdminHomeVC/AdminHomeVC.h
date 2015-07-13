a72fba1d221d45e3a00cf796d02defa74bde2893
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-07-13 09:24:08 +0300

    Added Admin Home View Controller

---
+++ Library Application with CoreData/Library Application with CoreData/Classes/Admin/AdminHomeVC/AdminHomeVC.h
@@ -0,0 +1,13 @@
+//
+//  AdminHomeVC.h
+//  Library Application with CoreData
+//
+//  Created by Ömer Emre Aslan on 13/07/15.
+//  Copyright © 2015 omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AdminHomeVC : UIViewController
+
+@end
