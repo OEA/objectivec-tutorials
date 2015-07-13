410f475ccc4b37b97148710bb6e8e446068b904a
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-07-13 14:34:57 +0300

    Added User Detail

---
+++ Library Application with CoreData/Library Application with CoreData/Classes/UserVC/UserDetailVC/UserDetailVC.h
@@ -0,0 +1,16 @@
+//
+//  UserDetailVC.h
+//  Library Application with CoreData
+//
+//  Created by Ömer Emre Aslan on 13/07/15.
+//  Copyright © 2015 omer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface UserDetailVC : UITableViewController<UISearchResultsUpdating, UISearchControllerDelegate, NSFetchedResultsControllerDelegate>
+
+@property (nonatomic,strong) NSManagedObjectContext* managedObjectContext;
+
+@end
---
+++ Library Application with CoreData/Library Application with CoreData/Classes/UserVC/UserDetailVC/UserDetailVC.m
@@ -0,0 +1,129 @@
+//
+//  UserDetailVC.m
+//  Library Application with CoreData
+//
+//  Created by Ömer Emre Aslan on 13/07/15.
+//  Copyright © 2015 omer. All rights reserved.
+//
+
+#import "UserDetailVC.h"
+
+@interface UserDetailVC ()
+@property (strong, nonatomic) UISearchController *searchController;
+@property (strong, nonatomic) NSMutableArray *logs;
+@property (strong, nonatomic) NSString *username;
+@end
+
+@implementation UserDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.searchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    self.searchController.searchResultsUpdater = self;
+    self.searchController.dimsBackgroundDuringPresentation = false;
+    self.searchController.delegate = self;
+    [self.searchController.searchBar sizeToFit];
+    self.tableView.tableHeaderView = self.searchController.searchBar;
+    [self initLogs];
+    [self.tableView reloadData];
+}
+
+
+#pragma mark - Core Data method
+
+- (NSManagedObjectContext *)managedObjectContext
+{
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
+- (void)initLogs
+{
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"UserLog"];
+    //request.predicate = [NSPredicate predicateWithFormat:@"user.username = %@", self.username];
+    
+    NSError *searchError;
+    
+    self.logs = [self.managedObjectContext executeFetchRequest:request error:&searchError];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController
+{
+    
+}
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.logs.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"detailCell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = @"KOD";
+    cell.detailTextLabel.text = @"İşlem detayı";
+    
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
