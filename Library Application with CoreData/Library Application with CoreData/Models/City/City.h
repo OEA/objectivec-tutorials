143f50a117d442a2313ce23e968cf8a09ec1b843
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-07-27 14:08:55 +0300

    Added City Model

---
+++ Library Application with CoreData/Library Application with CoreData/Models/City/City.h
@@ -0,0 +1,14 @@
+//
+//  City.h
+//  Library Application with CoreData
+//
+//  Created by Ömer Emre Aslan on 27/07/15.
+//  Copyright © 2015 omer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface City : NSObject
+@property (strong, nonatomic) NSString *name;
+@property (nonatomic) NSInteger count;
+@end
---
+++ Library Application with CoreData/Library Application with CoreData/Models/City/City.m
@@ -0,0 +1,13 @@
+//
+//  City.m
+//  Library Application with CoreData
+//
+//  Created by Ömer Emre Aslan on 27/07/15.
+//  Copyright © 2015 omer. All rights reserved.
+//
+
+#import "City.h"
+
+@implementation City
+
+@end
